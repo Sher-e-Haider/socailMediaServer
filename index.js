9129a5800cafb4449d28c2909d9af23066c329cc
md sahil raza <razasahil263@gmail.com>
2022-05-11 18:26:07 +0530

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ import AuthUser from './routes/authRoute.js'
 dotenv.config()
 const app = express()
 app.use(cors())
-app.use(express.json({limit: '30mb'}))
+//{limit: '30mb'}
+app.use(express.json())
 app.use(bodyParser.json({limit:'30mb',extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
 app.use('/auth',AuthUser)
